aefd9576c829c24684fdf56acabf9c8cadcf7068
harmolipi <nikob381@gmail.com>
2022-02-22 10:46:13 -0500

    Added export and fixed paths

--- routes/api.js
+++ routes/api.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const router = express.Router();
 
-const user_controller = require('../controller/userController');
-const exercise_controller = require('../controller/exercisesController');
+const user_controller = require('../controllers/userController');
+const exercise_controller = require('../controllers/exerciseController');
 
 router.get('/users/:id/logs', user_controller.get_logs);
 router.post('/users', user_controller.post_create_user);
 router.post('/users/:id/exercises', exercise_controller.post_create_exercise);
+
+module.exports = router;
