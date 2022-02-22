30e78f46b8803d864080a21a9c10a9393b42fe17
harmolipi <nikob381@gmail.com>
2022-02-22 11:30:43 -0500

    Added logging success message to console

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,12 +1,16 @@
 const User = require('../models/user');
 
 exports.post_create_user = (req, res) => {
-  console.log(req.query.username);
+  console.log(`creating: ${req.query.username}`);
+
   const user = new User({
     username: req.query.username,
   });
+
   user.save((err) => {
     if (err) return console.log(err);
+    console.log(`User '${user.username}' created successfully!`);
+
     res.json({
       username: user.username,
       id: user._id,
