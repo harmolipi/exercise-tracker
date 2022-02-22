f5a12561a27e51893efbdf058992970b11da2f43
harmolipi <nikob381@gmail.com>
2022-02-22 12:53:28 -0500

    Changed id to _id

--- routes/api.js
+++ routes/api.js
@@ -4,9 +4,9 @@ const router = express.Router();
 const user_controller = require('../controllers/userController');
 const exercise_controller = require('../controllers/exerciseController');
 
-router.get('/users/:id/logs', user_controller.get_logs);
+router.get('/users/:_id/logs', user_controller.get_logs);
+router.post('/users/:_id/exercises', exercise_controller.post_create_exercise);
 router.get('/users', user_controller.get_users);
 router.post('/users', user_controller.post_create_user);
-router.post('/users/:_id/exercises', exercise_controller.post_create_exercise);
 
 module.exports = router;
