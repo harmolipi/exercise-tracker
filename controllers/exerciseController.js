311cd356e56a79773ba16fc07f72c4f1507e958e
harmolipi <nikob381@gmail.com>
2022-02-22 11:27:08 -0500

    Built post_create_exercise function

--- controllers/exerciseController.js
+++ controllers/exerciseController.js
@@ -1,3 +1,24 @@
-exports.post_create_exercise = (req, res) => {
-  res.send('create exercise');
+const User = require('../models/user');
+const Exercise = require('../models/exercise');
+
+exports.post_create_exercise = async (req, res) => {
+  const user = await User.findById(req.params.id);
+
+  const exercise = new Exercise({
+    user: user._id,
+    description: req.query.description,
+    duration: req.query.duration,
+    date: req.query.date,
+  });
+
+  exercise.save((err) => {
+    if (err) return console.log(err);
+    res.json({
+      _id: user._id,
+      username: user.username,
+      date: exercise.date_formatted,
+      duration: exercise.duration,
+      description: exercise.description,
+    });
+  });
 };
