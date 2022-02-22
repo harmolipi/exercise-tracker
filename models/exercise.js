082ebaa36af4f1c4a90efbb189f967bbcd6ea12a
harmolipi <nikob381@gmail.com>
2022-02-22 10:53:56 -0500

    Added user ref to exercise schema

--- models/exercise.js
+++ models/exercise.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const ExerciseSchema = new Schema({
+  user: { type: Schema.Types.ObjectId, ref: 'User' },
   description: { type: String, required: true },
   duration: { type: Number, required: true },
   date: { type: Date },
