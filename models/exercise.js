73a49258e215e6975fc03c9e1b8b985db387a021
harmolipi <nikob381@gmail.com>
2022-02-22 10:54:55 -0500

    Require user for exercise

--- models/exercise.js
+++ models/exercise.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const ExerciseSchema = new Schema({
-  user: { type: Schema.Types.ObjectId, ref: 'User' },
+  user: { type: Schema.Types.ObjectId, ref: 'User', required: true },
   description: { type: String, required: true },
   duration: { type: Number, required: true },
   date: { type: Date },
