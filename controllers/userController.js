566e6627a07ea145b9bda795c3bb843d0126237f
harmolipi <nikob381@gmail.com>
2022-02-22 10:52:45 -0500

    Made post_create_user function

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,7 +1,16 @@
-exports.get_logs = (req, res) => {
-  res.send('get logs');
-};
+const User = require('../models/user');
 
 exports.post_create_user = (req, res) => {
-  res.send('create user');
+  console.log(req.query.username);
+  const user = new User({
+    username: req.query.username,
+  });
+  user.save((err) => {
+    if (err) return console.log(err);
+    res.send(`user created: ${user.username}`);
+  });
+};
+
+exports.get_logs = (req, res) => {
+  res.send('get logs');
 };
